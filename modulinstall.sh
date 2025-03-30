4cb147034ca48897cc88e4000ca3d8ca79b2650b
BielZcode <gabrieloliveirabrito62@gmail.com>
2025-03-30 09:45:32 -0300

    Update modulinstall.sh

--- modulinstall.sh
+++ modulinstall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rm -f astrodata.sh astrocreate.sh astroteste.sh astroremove.sh delete.py sincronizar.py add.sh rem.sh addteste.sh addsinc.sh remsinc.sh
+rm -f astrodata.sh astrocreate.sh astroteste.sh astroremove.sh delete.py sincronizar.py limpeza.sh add.sh rem.sh addteste.sh addsinc.sh remsinc.sh
 wget -O astrocreate.sh "https://raw.githubusercontent.com/AstroNetBielZcode/AstroModulosL/refs/heads/main/astrocreate.sh"
 wget -O add.sh "https://raw.githubusercontent.com/AstroNetBielZcode/AstroModulosL/refs/heads/main/add.sh"
 wget -O remsinc.sh "https://raw.githubusercontent.com/AstroNetBielZcode/AstroModulosL/refs/heads/main/remsinc.sh"
@@ -10,8 +10,9 @@ wget -O addteste.sh "https://raw.githubusercontent.com/AstroNetBielZcode/AstroMo
 wget -O astroremove.sh "https://raw.githubusercontent.com/AstroNetBielZcode/AstroModulosL/refs/heads/main/astroremove.sh"
 wget -O delete.py "https://raw.githubusercontent.com/AstroNetBielZcode/AstroModulosL/refs/heads/main/delete.sh"
 wget -O astrodata.sh "https://raw.githubusercontent.com/AstroNetBielZcode/AstroModulosL/refs/heads/main/astrodata.sh"
-wget -O sincronizar.py "https://raw.githubusercontent.com/AstroNetBielZcode/AstroModulosL/refs/heads/main/sincronizar.sh"
-chmod 777 astrocreate.sh add.sh remsinc.sh addsinc.sh rem.sh astroteste.sh addteste.sh astroremove.sh delete.py astrodata.sh sincronizar.py
+wget -O sincronizar.py "https://raw.githubusercontent.com/AstroNetBielZcode/AstroModulosL/refs/heads/main/sincronizar.py"
+wget -O limpeza.sh "https://raw.githubusercontent.com/AstroNetBielZcode/AstroModulosL/refs/heads/main/limpeza.sh"
+chmod 777 astrocreate.sh add.sh remsinc.sh addsinc.sh rem.sh astroteste.sh addteste.sh astroremove.sh delete.py astrodata.sh sincronizar.py limpeza.sh
 apt install dos2unix
 dos2unix rem.sh
 wget "https://raw.githubusercontent.com/AstroNetBielZcode/AstroModulosL/refs/heads/main/verificador.sh" -O verificador.py 
