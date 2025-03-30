6b1d1d773c516412ffc47c67494da894fe059d81
BielZcode <gabrieloliveirabrito62@gmail.com>
2025-03-30 09:36:02 -0300

    Create limpeza.sh

---
+++ limpeza.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Definindo os arquivos a serem removidos
+arquivos=(
+    "add.sh"
+    "addsinc.sh"
+    "addteste.sh"
+    "astrocreate.sh"
+    "astrodata.sh"
+    "astroremove.sh"
+    "delete.py.sh"
+    "modulinstall.sh"
+    "rem.sh"
+    "remsinc.sh"
+    "sincronizar.sh"
+    "verificador.py"
+)
+
+# Remover os arquivos
+for arquivo in "${arquivos[@]}"; do
+    if [ -f "$arquivo" ]; then
+        rm -f "$arquivo"
+        echo "Arquivo $arquivo removido com sucesso."
+    else
+        echo "Arquivo $arquivo não encontrado."
+    fi
+done
+
+# Mensagem final
+echo "Todos os arquivos especificados foram processados."
+exit 0
